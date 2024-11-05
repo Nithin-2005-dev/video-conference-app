502621e0401b38e28610d7c86e00a32a4a681466
Nithin-2005-dev <nithinkumar2005b@gmail.com>
2024-11-05 19:12:04 +0530

    nav and sidebar added

---
+++ src/constants/index.ts
@@ -0,0 +1,25 @@
+export const sidebarLinks=[
+    {
+        imageUrl:'/icons/Home.svg',
+        route:'/',
+        label:'Home'
+    },
+    {
+        imageUrl:'/icons/upcoming.svg',
+        route:'/upcoming',
+        label:'Upcoming'
+    }, {
+        imageUrl:'/icons/previous.svg',
+        route:'/previous',
+        label:'Previous'
+    }, {
+        imageUrl:'/icons/Video.svg',
+        route:'/recording',
+        label:'Recording'
+    },
+    {
+        imageUrl:'/icons/add-personal.svg',
+        route:'/personal-room',
+        label:'Personal Room'
+    },
+]
